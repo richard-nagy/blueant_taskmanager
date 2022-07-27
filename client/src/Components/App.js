fcecc986902e1e56e0f3c7b119368479accac5ed
Richárd Nagy <nagy.richard@thisisdone.com>
2022-07-27 12:51:59 +0200

    Add table

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import TaskBar from "./TaskBar";
+import AddIcon from "@mui/icons-material/Add";
+import {
+    Chip,
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@mui/material";
 
 function App() {
     const [tasks, setTasks] = useState();
@@ -13,15 +24,34 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <ul>
-                {!tasks
-                    ? "Loading..."
-                    : tasks.map((e) => {
-                          return <li>{e.task}</li>;
-                      })}
-            </ul>
-        </div>
+        <Container>
+            <TableContainer>
+                <Table>
+                    <TableBody>
+                        {!tasks
+                            ? "Loading..."
+                            : tasks.map((object) => {
+                                  return <TaskBar object={object} key={object.idtasks} />;
+                              })}
+                        <TableRow>
+                            <TableCell colSpan={3}>
+                                <Chip
+                                    icon={<AddIcon style={{ color: "orangered" }} />}
+                                    label="Add task"
+                                    sx={{
+                                        backgroundColor: "white",
+                                        cursor: "pointer",
+                                        "&:hover": {
+                                            backgroundColor: "whitesmoke",
+                                        },
+                                    }}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
     );
 }
 
--- client/src/Components/TaskBar.js
+++ client/src/Components/TaskBar.js
@@ -1,3 +1,23 @@
-export default function TaskBar() {
-    return <div>() text </div>;
+import { Checkbox, Radio, TableCell, TableRow } from "@mui/material";
+import CircleCheckedFilled from "@mui/icons-material/CheckCircle";
+import CircleUnchecked from "@mui/icons-material/CircleOutlined";
+
+export default function TaskBar({ object }) {
+    return (
+        <TableRow>
+            <TableCell sx={{ width: "1px", whiteSpace: "nowrap", paddingRight: "0" }}>
+                <Checkbox
+                    icon={<CircleUnchecked />}
+                    checkedIcon={<CircleCheckedFilled />}
+                    size="small"
+                    color={object.color}
+                    sx={{
+                        color: `${object.color}.main`,
+                    }}
+                />
+            </TableCell>
+            <TableCell sx={{ paddingLeft: "0" }}>{object.task}</TableCell>
+            <TableCell align="right">{object.idusers ? object.idusers : "-"}</TableCell>
+        </TableRow>
+    );
 }
