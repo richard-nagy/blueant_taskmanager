f1645f3235e207e1b7ca1fcff30797d5be8c3b67
Richárd Nagy <nagy.richard@thisisdone.com>
2022-07-27 10:51:50 +0200

    Add starting files

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "0101",
+    database: "blueant_db",
+});
+
+/*
+TODO: Request the middlewares
+require("./users.js")(app, db);
+require("./tasks.js")(app, db);
+*/
+
+app.listen(3001, () => {
+    console.log(`Your server is running on port 3001...`);
+});
